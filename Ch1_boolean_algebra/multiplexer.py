f939f6f890cda2e5e1ba031bf6f1ffa9f683f901
chang <ckdgusss@gmail.com>
2024-03-13 15:05:15 +0900

    2024. 03. 13 implementation of 2bit demultiplexer

--- Ch1_boolean_algebra/multiplexer.py
+++ Ch1_boolean_algebra/multiplexer.py
@@ -3,3 +3,7 @@ import gate
 
 def mux_2bit(a, b, sel):
     return gate.or_gate(gate.and_gate(gate.not_gate(sel), a), gate.and_gate(sel, b))
+
+
+def demux_2bit(a, sel):
+    return [gate.and_gate(a, gate.not_gate(sel)), gate.and_gate(a, sel)]
