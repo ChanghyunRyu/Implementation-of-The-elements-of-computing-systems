c7ece869d08b42539e90525016c8c927d0f9b537
chang <ckdgusss@gmail.com>
2024-03-19 13:32:01 +0900

    2024. 03. 19 - 2 implementation of 2bit register

--- Ch3_memory/sequential_chip.py
+++ Ch3_memory/sequential_chip.py
@@ -1,4 +1,7 @@
 from Ch1_boolean_algebra import gate
+from Ch1_boolean_algebra import multiplexer
+# Originally, data flip-flop and register don't have initial value.
+# Since we did not assume the case of entering the 'None' value into the existing chip, initial value is set to 0.
 
 
 # Chip name: DFF
@@ -12,3 +15,16 @@ class dff:
         self.q = gate.nand_gate(self.q_bar, gate.nand_gate(clk, d))
         self.q_bar = gate.nand_gate(self.q, gate.nand_gate(clk, gate.not_gate(d)))
         return self.q
+
+
+# Chip name: register
+# input: d, load , output: out,
+# function: if load(t) then out(t+1) = d(t), else out(t+1) = out(t)
+class register:
+    def __init__(self):
+        self.out = 0
+        self.dff = dff()
+
+    def circuit(self, d, load, clk):
+        self.out = self.dff.circuit(multiplexer.mux_2bit(self.out, d, load), clk)
+        return self.out
--- Ch3_memory/simulation.py
+++ Ch3_memory/simulation.py
@@ -1,5 +1,6 @@
 import sequential_chip
 
+print('-------------dff---------------')
 dff1 = sequential_chip.dff()
 inputs = [1, 0, 0, 1, 0, 1]
 time = -1
@@ -9,3 +10,16 @@ for i in range(12):
     if clk == 0:
         time += 1
         print('time = {}, input = {}, q = {}'.format(time+1, inputs[time], dff1.q))
+print()
+
+print('-------------register---------------')
+rgs1 = sequential_chip.register()
+inputs = [1, 0, 0, 0, 0, 0]
+loads = [1, 0, 0, 1, 0, 0]
+time = -1
+for i in range(12):
+    clk = i % 2
+    rgs1.circuit(inputs[time], loads[time], clk)
+    if clk == 0:
+        time += 1
+        print('time = {}, load = {}, input = {}, out = {}'.format(time+1, loads[time], inputs[time], rgs1.out))
